4e5ac9355dbb1ae32037dfc3e59cc7c7a47e54d2
jvrieling <32886119+jvrieling@users.noreply.github.com>
2017-10-18 15:52:50 -0400

    Update Assign1.sql

--- Assign1.sql
+++ Assign1.sql
@@ -1,3 +1,5 @@
+SET PAGESIZE 100;
+
 -- QUESTION 1
 select
 cust_no, cname, country_cd
@@ -38,3 +40,50 @@ and c.country_cd = 'UK'
 and upper(c.city) like 'L%'
 and o.order_no < 75
 order by c.cust_no desc;
+
+--Question 6
+select
+count(country_id)
+from countries;
+
+--Question 7
+select
+c.cname, ol.order_no, price*qty as TOTAL
+from orderlines ol, orders o, customers c
+where ol.order_no = o.order_no
+and o.cust_no = c.cust_no
+and upper(c.city) = 'LONDON'
+order by o.order_no;
+
+--Question 8
+select
+employee_id, cast(last_name || ' ' || first_name as varchar(25)) as "Full Name",
+job_id, to_char(hire_date, 'Month ddth "of year" yyyy') as "Start Date"
+from employees
+--NOTE: I can't use MONTH() or YEAR() Functions! It just says invalid identifier...
+--And using upper(to_char()) = 'MAY' doesn't work either...
+where (to_char(hire_date, 'Month') like 'May%'
+or to_char(hire_date, 'Month') like 'June%'
+or to_char(hire_date, 'Month') like 'July%'
+or to_char(hire_date, 'Month') like 'August%'
+or  to_char(hire_date, 'Month') like 'December%')
+order by hire_date;
+
+--Question 9
+select
+'Employee ' || employee_id || ' named ' ||
+cast(last_name || ' ' || first_name as varchar(25)) ||
+' with Job ID of '  || job_id || ' will have new salary of ' || 
+case
+    when upper(job_id) like '%_MAN' then salary*1.3
+    when upper(job_id) like '%_VP' then salary*1.2
+end as "Employees with Increased Pay"
+from employees
+where salary/12 not between 6000 and 11000
+and (job_id like '%_MAN'
+or job_id like '%_VP')
+order by salary;
+
+--Question 10
+
+
