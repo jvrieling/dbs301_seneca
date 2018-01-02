43307b6267702a51e39ea9df2d4183be1a647442
jvrieling <32886119+jvrieling@users.noreply.github.com>
2018-01-02 13:37:42 -0500

    Update Assign3.sql

--- Assign3.sql
+++ Assign3.sql
@@ -55,3 +55,51 @@ ADD CONSTRAINT warehouse_ck CHECK(rating LIKE'[A-DF]');
 
 
 --QUESTION 4
+CREATE SEQUENCE Whse_id_seq
+INCREMENT BY 15
+START WITH 320
+MAXVALUE 900
+NOCACHE
+NOCYCLE;
+
+-- QUESTION 5
+INSERT INTO warehouse
+values(
+whse_id_seq.NEXTVAL,
+'ATLANTA',
+null,
+sysdate
+);
+
+--QUESTION 6
+CREATE TABLE CITIES AS
+SELECT * FROM locations
+WHERE  location_id < 2000;
+
+--QUESTION 7
+DESCRIBE cities;
+
+--QUESTION 8
+SLECT * FROM cities;
+
+--OUTPUT:
+1000	1297 Via Cola di Rie	00989	Roma		IT
+1100	93091 Calle della Testa	10934	Venice		IT
+1200	2017 Shinjuku-ku	1689	Tokyo	Tokyo Prefecture	JP
+1300	9450 Kamiya-cho	6823	Hiroshima		JP
+1400	2014 Jabberwocky Rd	26192	Southlake	Texas	US
+1500	2011 Interiors Blvd	99236	South San Francisco	California	US
+1600	2007 Zagora St	50090	South Brunswick	New Jersey	US
+1700	2004 Charade Rd	98199	Seattle	Washington	US
+1800	147 Spadina Ave	M5V 2L7	Toronto	Ontario	CA
+1900	6092 Boxwood St	YSW 9T2	Whitehorse	Yukon	CA
+
+--QUESTION 9
+CREATE OR REPLACE VIEW whssec_man_vu
+(warehouse_id, section_id, city, groupz, LName)
+AS(
+SELECT s.warehouse_id, s.section_id, w.city, d.division_id, e.last_name
+FROM section s, employees e, warehouse w, division d
+WHERE e.employee_id = s.mgr_id
+AND s.warehouse_id = w.warehouse_id
+);
